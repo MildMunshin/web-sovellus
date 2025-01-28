bcd3f343afbfe2febc29904a6c51a7622c16cd4c
Samuli Laiho <samuli.laiho@helsinki.fi>
2025-01-28 11:54:18 +0200

    add users.py and update app.py and database.db

--- app.py
+++ app.py
@@ -55,3 +55,23 @@ def login():
 def logout():
     del session["username"]
     return redirect("/")
+
+@app.route("/add_image", methods=["GET", "POST"])
+def add_image():
+    require_login()
+
+    if request.method == "GET":
+        return render_template("add_image.html")
+
+    if request.method == "POST":
+        file = request.files["image"]
+        if not file.filename.endswith(".jpg"):
+            return "VIRHE: väärä tiedostomuoto"
+
+        image = file.read()
+        if len(image) > 100 * 1024:
+            return "VIRHE: liian suuri kuva"
+
+        user_id = session["user_id"]
+        users.update_image(user_id, image)
+        return redirect("/user/" + str(user_id))
