ed5f36577304be0b5a5d2476fec1e0df1fe12aaa
Samuli Laiho <samuli.laiho@helsinki.fi>
2025-01-30 12:25:53 +0200

    fixing database.db and schema.sql

--- schema.sql
+++ schema.sql
@@ -5,16 +5,18 @@ CREATE TABLE visits (
 CREATE TABLE users (
     id INTEGER PRIMARY KEY,
     username TEXT UNIQUE,
-    password_hash TEXT
-, image BLOB);
+    password_hash TEXT,
+    image BLOB
+);
 CREATE TABLE songs (
     id INTEGER PRIMARY KEY,
     user_id INTEGER NOT NULL,
     title TEXT NOT NULL,
     artist TEXT NOT NULL CHECK (LENGTH(artist) > 0),
-    file_path TEXT NOT NULL,
+    audio_file_path TEXT NOT NULL,
+    image_file_path TEXT NOT NULL,
     upload_date DATETIME DEFAULT CURRENT_TIMESTAMP,
     genre TEXT CHECK (LENGTH(genre) > 0),
-    duration INTEGER CHECK (duration > 0),
+    duration INTEGER CHECK (duration > 0), cover_image TEXT,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
