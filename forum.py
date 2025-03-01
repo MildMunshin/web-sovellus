4c3a0d1b97cdc5808ed8c8bac8760f49397adad1
Samuli Laiho <samuli.laiho@helsinki.fi>
2025-03-01 16:23:37 +0200

    cleaning up unused code

--- forum.py
+++ forum.py
@@ -35,12 +35,3 @@ def update_message(message_id, content):
 def delete_message(message_id):
     sql = "DELETE FROM messages WHERE id = ?"
     db.execute(sql, [message_id])
-
-
-# def get_threads(id):
-#     sql = """SELECT t.id, t.title, COUNT(m.id) total, MAX(m.sent_at) last
-#              FROM threads t, messages m, songs s
-#              WHERE t.id = s.song_id
-#              GROUP BY t.id
-#              ORDER BY t.id DESC"""
-#     return db.query(sql)
